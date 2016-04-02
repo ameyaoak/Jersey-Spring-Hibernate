f8a8b25baf9a24e18332a2fa1552bc31880024db
Oak, Ameya <Ameya.Oak@cdk.com>
2016-04-02 08:37:46 +0530

    changes for invoice

--- src/main/java/com/jersey/series/spring/hibernate/model/Invoiced.java
+++ src/main/java/com/jersey/series/spring/hibernate/model/Invoiced.java
@@ -9,8 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.ForeignKey;
-import org.hibernate.annotations.GenericGenerator;
+import com.jersey.series.spring.hibernate.utils.CommonUtils;
 
 @Entity  
 @Table(name="invoiced")
@@ -19,64 +18,83 @@ public class Invoiced {
 	@SequenceGenerator(name="invoiceSeq", initialValue=1, allocationSize=100)
 	@GeneratedValue(strategy=GenerationType.AUTO, generator="invoiceSeq") 
 	Long invoiceNo;
-	
-	
-	String inwardNo,dispatchId;
-	
+	String inwardNo,dispatchId,party,component, material;
+	 
 	Double qtyKgs,  qtyNos, rateKg,  rateNos,total;
 	Date creationDate;
+	
 	public String getInvoiceNo() {
 		return invoiceNo.toString();
-	}
-	public void setInvoiceNo(String invoiceNo) {
-		this.invoiceNo = Long.parseLong(invoiceNo);
 	}
 	public String getInwardNo() {
 		return inwardNo;
 	}
-	public void setInwardNo(String inwardNo) {
-		this.inwardNo = inwardNo;
-	}
 	public String getDispatchId() {
 		return dispatchId;
 	}
-	public void setDispatchId(String dispatchId) {
-		this.dispatchId = dispatchId;
+	public String getParty() {
+		return party;
+	}
+	public String getComponent() {
+		return component;
+	}
+	public String getMaterial() {
+		return material;
 	}
 	public Double getQtyKgs() {
 		return qtyKgs;
 	}
-	public void setQtyKgs(Double qtyKgs) {
-		this.qtyKgs = qtyKgs;
-	}
 	public Double getQtyNos() {
 		return qtyNos;
-	}
-	public void setQtyNos(Double qtyNos) {
-		this.qtyNos = qtyNos;
 	}
 	public Double getRateKg() {
 		return rateKg;
 	}
-	public void setRateKg(Double rateKg) {
-		this.rateKg = rateKg;
-	}
 	public Double getRateNos() {
 		return rateNos;
-	}
-	public void setRateNos(Double rateNos) {
-		this.rateNos = rateNos;
 	}
 	public Double getTotal() {
 		return total;
 	}
+	public String getCreationDate() {
+		return CommonUtils.formatDate(creationDate);
+	}
+	public void setInvoiceNo(String invoiceNo) {
+		this.invoiceNo = Long.parseLong(invoiceNo);
+	}
+	public void setInwardNo(String inwardNo) {
+		this.inwardNo = inwardNo;
+	}
+	public void setDispatchId(String dispatchId) {
+		this.dispatchId = dispatchId;
+	}
+	public void setParty(String party) {
+		this.party = party;
+	}
+	public void setComponent(String component) {
+		this.component = component;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public void setQtyKgs(Double qtyKgs) {
+		this.qtyKgs = qtyKgs;
+	}
+	public void setQtyNos(Double qtyNos) {
+		this.qtyNos = qtyNos;
+	}
+	public void setRateKg(Double rateKg) {
+		this.rateKg = rateKg;
+	}
+	public void setRateNos(Double rateNos) {
+		this.rateNos = rateNos;
+	}
 	public void setTotal(Double total) {
 		this.total = total;
-	}
-	public Date getCreationDate() {
-		return creationDate;
 	}
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
+	
+	 
 }
